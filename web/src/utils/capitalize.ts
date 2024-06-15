c44be37a420570701d183dfed4ea4e8a49c364de
João Gabriel <joao.bicalho@dcc.ufmg.br>
2024-06-14 23:44:32 -0300

    fixes

--- web/src/utils/capitalize.ts
+++ web/src/utils/capitalize.ts
@@ -1,9 +1,12 @@
-export function capitalize(str: string) {
+export function capitalize(str: string, replaceUndescores = false) {
   if (!str || typeof str !== 'string') {
     return ''
   }
+
+  if (replaceUndescores) str = str.replaceAll('_', ' ')
+
   return str
     .split(' ')
-    .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
     .join(' ')
 }
