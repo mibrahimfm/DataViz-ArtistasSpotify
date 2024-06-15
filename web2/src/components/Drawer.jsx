b0f84378ae4d50f65d29910746f85cc132b1d1fe
João Gabriel <joao.bicalho@dcc.ufmg.br>
2024-06-15 10:49:36 -0300

    bold href

--- web2/src/components/Drawer.jsx
+++ web2/src/components/Drawer.jsx
@@ -1,9 +1,12 @@
 'use client';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
 import { useDrawer } from '../../hooks/use-drawer';
 
 export default function Drawer({ links }) {
   const { isExpanded } = useDrawer();
+
+  const pathname = usePathname();
 
   return (
     <div
@@ -16,7 +19,10 @@ export default function Drawer({ links }) {
       </div>
       <ul className="space-y-2">
         {links.map((l) => (
-          <li key={l.h}>
+          <li
+            key={l.h}
+            style={{ fontWeight: pathname === l.h ? '700' : '500' }}
+          >
             <Link
               className="block text-gray-700 dark:text-gray-300 hover:text-[#0D9D55] transition-colors"
               href={l.h}
